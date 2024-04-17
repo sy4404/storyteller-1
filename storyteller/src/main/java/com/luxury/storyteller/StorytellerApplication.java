81f234e6dfa85f48a06dccb8f7ec634d1f11751c
박소영 <48235429+soy6032@users.noreply.github.com>
2024-04-17 14:01:02 +0900

    boot start

---
+++ storyteller/src/main/java/com/luxury/storyteller/StorytellerApplication.java
@@ -0,0 +1,13 @@
+package com.luxury.storyteller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorytellerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StorytellerApplication.class, args);
+	}
+
+}
