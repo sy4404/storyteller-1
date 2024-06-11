8227b88b104b8a5b180553c44ef07f52ef520cf5
박소영 <48235429+soy6032@users.noreply.github.com>
2024-06-11 12:13:12 +0900

    examinationDto

--- storyteller/src/main/java/com/luxury/storyteller/dto/ExaminationDto.java
+++ storyteller/src/main/java/com/luxury/storyteller/dto/ExaminationDto.java
@@ -6,6 +6,24 @@ import lombok.ToString;
 @Data
 @ToString
 public class ExaminationDto {
+    private int examinationIdx;
+    private String question;
+
+    private int examinationAnswerIdx;
+    private int isAnswer;
+
     private int examinationChapterIdx;
     private String name;
+
+    private int examinationMajorIdx;
+    private String examinationMajorTitle;
+
+    private int examinationSelectIdx;
+    private int examinationSelectNum;
+    private String examinationSelectTitle;
+
+    private int userExaminationResultIdx;
+    private int userIdx;
+    private int score;
+    private String createdAt;
 }
