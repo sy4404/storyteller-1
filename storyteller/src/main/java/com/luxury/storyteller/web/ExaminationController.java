8eaf1c6c4c8557448aa19a0fb13f97b8555edc10
박소영 <48235429+soy6032@users.noreply.github.com>
2024-06-14 08:39:47 +0900

    main

--- storyteller/src/main/java/com/luxury/storyteller/web/ExaminationController.java
+++ storyteller/src/main/java/com/luxury/storyteller/web/ExaminationController.java
@@ -1,9 +1,12 @@
 package com.luxury.storyteller.web;
 
 import com.luxury.storyteller.dto.CommunityDto;
+import com.luxury.storyteller.dto.ExaminationDto;
 import com.luxury.storyteller.service.community.CommunityService;
+import com.luxury.storyteller.service.examination.ExaminationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +18,7 @@ import java.util.List;
 @RequestMapping("/examination")
 public class ExaminationController {
 
+    private final ExaminationService examinationService;
     /**
      * 시험
      */
@@ -22,16 +26,30 @@ public class ExaminationController {
     private final CommunityService communityService;
 
     @GetMapping("")
-    public String examinationBefore() {
+    public String examinationBefore(Model model) {
 
+        List<ExaminationDto> list = examinationService.findExaminationMajorAll();
+        model.addAttribute("lists", list);
 
         return "examination/before";
     }
 
-    @GetMapping("{communityIdx}")
-    public String examinationList(@PathVariable int communityIdx) {
-
-
+    @GetMapping("{examinationMajorIdx}")
+    public String examinationList(@PathVariable int examinationMajorIdx, Model model) {
+        List<ExaminationDto> list = examinationService.findExaminationChapterByExaminationMajorIdx(examinationMajorIdx);
+        model.addAttribute("lists", list);
         return "examination/list";
+    }
+
+    @GetMapping("/ex/{examinationIdx}")
+    public String examinationDetail(@PathVariable int examinationIdx, Model model) {
+        ExaminationDto detail = examinationService.findExaminationByExaminationIdx(examinationIdx);
+        List<ExaminationDto> list = examinationService.findexaminationSelectByExaminationIdx(examinationIdx);
+
+        model.addAttribute("detail", detail);
+        model.addAttribute("lists", list);
+
+        model.addAttribute("num", examinationIdx);
+        return "examination/detail";
     }
 }
